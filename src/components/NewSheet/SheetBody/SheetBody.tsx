b2ffbe8e96aea78f65c00e361a90392a818cc6f1
Mohammad <mo.sdghn@gmail.com>
2024-03-03 12:51:34 +0330

    [refactor] import types from types folder

--- src/components/NewSheet/SheetBody/SheetBody.tsx
+++ src/components/NewSheet/SheetBody/SheetBody.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { PropsWithChildren, useLayoutEffect, useRef } from "react";
-import { Phase } from "../BottomSheet/BottomSheet.tsx";
+import { Phase } from "@/../types/phase.ts";
 import { useScroll } from "@use-gesture/react";
 
 interface Props extends PropsWithChildren {
