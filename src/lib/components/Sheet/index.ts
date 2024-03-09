a75622daabf6506897ee6c5118976faf3d2b474d
Mohammad <mo.sdghn@gmail.com>
2024-03-09 11:45:31 +0330

    [chore] export components from index.ts file

--- src/lib/components/Sheet/index.ts
+++ src/lib/components/Sheet/index.ts
@@ -1,3 +1,5 @@
 import Sheet from "./Sheet.tsx";
 
+export { default as SheetNoHead } from "./SheetNoHead.tsx";
+export { default as SheetWithHead } from "./SheetWithHead.tsx";
 export default Sheet;
