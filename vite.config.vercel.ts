05a823647e4dd0302e51a72f211617a5bf505b30
Mohammad <mo.sdghn@gmail.com>
2024-07-14 16:47:09 +0330

    [refactor] update lib folder path in configs

--- vite.config.vercel.ts
+++ vite.config.vercel.ts
@@ -1,18 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import typescript from "@rollup/plugin-typescript";
 import path from "path";
 
 export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "@lib": path.resolve(__dirname, "./src/lib"),
+      "@lib": path.resolve(__dirname, "./lib"),
       "@appTypes": path.resolve(__dirname, "./types"),
       "@examples": path.resolve(__dirname, "./examples"),
     },
   },
-  plugins: [react(), typescript()],
+  plugins: [react()],
   build: {
     outDir: "./dist-vercel",
   },
