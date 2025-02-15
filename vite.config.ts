9e1105594ab4cda93befdd2e132a8429af25f510
mo-mini-mac <mo.sdghn@gmail.com>
2025-02-15 13:23:19 +0330

    [feat] add rollup visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
+import { visualizer } from "rollup-plugin-visualizer";
 import path from "path";
 
 export default defineConfig({
@@ -18,6 +19,7 @@ export default defineConfig({
       include: ["lib", "types"],
     }),
     react(),
+    visualizer(),
   ],
   build: {
     copyPublicDir: false,
